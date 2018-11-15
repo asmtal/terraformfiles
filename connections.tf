01da3f8743a9d525d7b38c4961577cb381cbf5d7
deepshikharai <37527448+deepshikharai@users.noreply.github.com>
2018-11-15 16:02:27 +0530

    updated project id

--- connections.tf
+++ connections.tf
@@ -1,6 +1,6 @@
 
 provider "google" {
     credentials = "${file("account.json")}"
-    project     = "skilled-module-222310"
+    project     = "superb-ascent-222607"
     region      = "us-east1"
 }
